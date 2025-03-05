9ec6da982325ba5ed12c4ac4d72e4e767faac290
rodriguezsandrap05378 <rodriguez_sandrap05378@gmx.com>
2025-03-05 18:39:13 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Ask user for their name
+    cout << "What is your name?" << endl;
+    string name;
+    cin >> name;
+
+    // Greet the user by name
+    cout << "Hello, " << name << "!" << endl;
+
+    // Print a message to the user
+    cout << "Welcome to the school project!" << endl;
+
+    return 0;
+}
